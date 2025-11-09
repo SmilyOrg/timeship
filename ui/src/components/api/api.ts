e59fcb416a0eea7623ac6a1b72a5baa5af335c2a
Miha Lunar <mlunar@gmail.com>
2025-11-09 19:21:04 +0100

    feat: add useApis function for handling multiple API queries and enhance type definitions

--- ui/src/components/api/api.ts
+++ ui/src/components/api/api.ts
@@ -1,7 +1,23 @@
-import { useQuery } from "@tanstack/vue-query";
+import { useQueries, useQuery } from "@tanstack/vue-query";
+import { computed, watch, type Ref } from "vue";
+
+export interface Snapshot {
+  id: string;
+  timestamp: string;
+  type: string;
+}
+
+export interface Node {
+  basename: string;
+  extension: string;
+  file_size: number;
+  last_modified: number;
+  path: string;
+  type: string;
+}
 
 // Uses useQuery for a generic API implementation
-function useApi(endpoint: string) {
+export function useApi(endpoint: string) {
   return useQuery({
     queryKey: [endpoint],
     queryFn: async () => {
@@ -14,4 +30,41 @@ function useApi(endpoint: string) {
   });
 }
 
-export { useApi };
+export function useApis(endpoints: Ref<string[]>) {
+  const queries = computed(() => {
+    console.log("Endpoints:", endpoints.value);
+    return endpoints.value.map(endpoint => ({
+      queryKey: [endpoint],
+      queryFn: async () => {
+        const response = await fetch(`http://localhost:8080${endpoint}`);
+        if (!response.ok) {
+          throw new Error('Request failed: ' + response.statusText);
+        }
+        return response.json();
+      },
+    }));
+  });
+  watch(queries, (newQueries) => {
+    console.log("Queries updated:", newQueries);
+  });
+  // const userQueries = useQueries({queries: queries})
+
+
+  // const queries = computed(() => {
+  //   return endpoints.value.map((endpoint) => ({
+  //     queryKey: [endpoint],
+  //     queryFn: async () => {
+  //       const response = await fetch(`http://localhost:8080${endpoint}`);
+  //       if (!response.ok) {
+  //         throw new Error('Request failed: ' + response.statusText);
+  //       }
+  //       return response.json();
+  //     },
+  //   }));
+  // });
+  const data = useQueries({ queries });
+  watch(data, (newData) => {
+    console.log("Data updated:", newData);
+  });
+  return data;
+}
