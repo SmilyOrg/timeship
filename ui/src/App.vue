4041c760f71b0dbddf13fc76cf91896c2ad9372c
Miha Lunar <mlunar@gmail.com>
2025-11-09 21:56:44 +0100

    feat: add logo image and update App.vue to include header with logo

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <main>
+    <header>
+      <img class="logo" src="/logo.png" alt="Timeship Logo" />
+    </header>
     <browser class="browser" />
   </main>
 </template>
@@ -9,6 +12,12 @@ import Browser from './components/Browser.vue';
 </script>
 
 <style scoped>
+
+.logo {
+  display: block;
+  height: 64px;
+}
+
 .browser {
   width: 100%;
   height: 600px;
