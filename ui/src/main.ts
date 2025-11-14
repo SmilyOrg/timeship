0fd8871bf8325bac16f8cea7f2ddc825bd7190b7
Miha Lunar <mlunar@gmail.com>
2025-11-14 02:43:16 +0100

    feat: add fomantic-ui-css dependency and import in main.ts

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'fomantic-ui-css/semantic.min.css';
 import App from './App.vue'
 
 import { VueQueryPlugin } from '@tanstack/vue-query'
